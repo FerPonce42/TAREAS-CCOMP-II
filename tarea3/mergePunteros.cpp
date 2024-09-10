816c82dc831dd4072b67fc0fd60b7d19efba8e59
FerPonce42 <angel.pacsi@ucsp.edu.pe>
2024-09-10 07:34:16 -0500

    mas simple la tarea 3

--- tarea3/mergePunteros.cpp
+++ tarea3/mergePunteros.cpp
@@ -1,70 +1,42 @@
 #include <iostream>
 using namespace std;
 
-void merge(int *pares_ptr, int *impares_ptr, int tamaño_total) {
-    int tamaño_pares = tamaño_total / 2;  
-    int tamaño_impares = tamaño_total - tamaño_pares;  
-    int *result = new int[tamaño_total]; 
-    int *result_ptr = result; 
-    int *final_pares = impares_ptr; 
-    int *final_impares = impares_ptr + tamaño_impares;  
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-    // Mezclamos mientras ambos punteros tengan elementos válidos
-    while (pares_ptr < final_pares && impares_ptr < final_impares) {
-        if (*pares_ptr < *impares_ptr) {
-            *result_ptr = *pares_ptr;  // Copiamos el valor menor desde los pares
-            pares_ptr++;      // Avanzamos el puntero de pares
-        } else {
-            *result_ptr = *impares_ptr;  // Copiamos el valor menor desde los impares
-            impares_ptr++;      // Avanzamos el puntero de impares
+void merge(int *inicio, int tamaño) {
+    int *ptr1 = inicio;
+    int *ptr2;
+
+    while (ptr1 < inicio + tamaño) {
+        ptr2 = ptr1 + 1; // se compara con el sigueinte
+        while (ptr2 < inicio + tamaño) {
+            if (*ptr1 > *ptr2) {
+                // Si ptr1 > Intercambio valores
+                swap(ptr1, ptr2);
+            }
+            ptr2++;  // next next
         }
-        result_ptr++;  // Avanzamos en el arreglo resultante
+        ptr1++;  // avanza el primero puntero
     }
-
-    // Si quedan elementos en la primera mitad (pares)
-    while (pares_ptr < final_pares) {
-        *result_ptr = *pares_ptr;
-        pares_ptr++;
-        result_ptr++;
-    }
-
-    // Si quedan elementos en la segunda mitad (impares)
-    while (impares_ptr < final_impares) {
-        *result_ptr = *impares_ptr;
-        impares_ptr++;
-        result_ptr++;
-    }
-
-    // Copiamos el contenido de result de vuelta al arreglo original usando punteros
-    pares_ptr -= tamaño_pares;  // Regresamos el puntero pares_ptr al inicio del arreglo original
-    result_ptr = result;  // Reiniciamos el puntero result_ptr al inicio del arreglo temporal
-    while (tamaño_total--) {
-        *pares_ptr = *result_ptr;  // Copiamos el valor de result al arreglo original
-        pares_ptr++;
-        result_ptr++;
-    }
-
-    delete[] result;  // Liberamos la memoria del arreglo temporal
 }
 
 int main() {
-  
-    int arr[] = {2, 6, 8, 12, 20, 1, 3, 7, 11, 15};
+    int arr[] = {12, 2, 20, 8, 6, 15, 3, 7, 11, 1};
     int tamaño = sizeof(arr) / sizeof(arr[0]);
 
+    
+    merge(arr, tamaño);
 
-    int *pares_ptr = arr;         // Puntero al inicio del arreglo
-    int *impares_ptr = arr + tamaño / 2;  // Puntero al inicio de la segunda mitad
-
-
-    merge(pares_ptr, impares_ptr, tamaño);
-
-    // Imprimimos 
-    cout << "ELIJO CREER = ";
-    int *ptr = arr;  // Puntero al inicio del arreglo
+ 
+    cout << "DECIDO CREER ";
+    int *ptr = arr;  // Puntero al inicio 
     while (ptr < arr + tamaño) {
-        cout << *ptr << " ";  // Imprimimos el valor al que apunta ptr
-        ptr++;  // Avanzamos al siguiente elemento
+        cout << *ptr << " "; 
+        ptr++; 
     }
     cout << endl;
 
