2766558d39971a5fe432cb4c01d907e8907eeffb
FerPonce42 <angel.pacsi@ucsp.edu.pe>
2024-09-28 14:30:45 -0500

    split que ORDENA

--- TAREAS/tarea4/split.cpp
+++ TAREAS/tarea4/split.cpp
@@ -3,18 +3,17 @@ using namespace std;
 
 void split(int* p, int* q) {
     while (p < q) {
+        
         if (*p % 2 == 0) {
             p++;
         }
-
+       
         else if (*q % 2 == 1) {
             q--;
         }
-
+       
         else {
-            *p = *p + *q; 
-            *q = *p - *q;
-            *p = *p - *q;
+            swap(*p, *q);
             p++;
             q--;
         }
@@ -22,9 +21,9 @@ void split(int* p, int* q) {
 }
 
 int main() {
-    int A[] = {3,7,2,6,12,35,8,10,99};
-    int* p = A;  
-    int* q = A + (sizeof(A) / sizeof(A[0])) - 1; 
+    int A[] = {3,7,2,6,12,35,8,10,99}; // ESTO IMPRIME: 10 8 2 6 12 35 7 3 99
+    int* p = A;
+    int* q = A + (sizeof(A) / sizeof(A[0])) - 1;
 
     cout << "ANTES:  ";
     for (int* ptr = p; ptr <= q; ++ptr) {
@@ -34,8 +33,8 @@ int main() {
 
     split(p, q);
 
-    cout << "DESPU:  ";
-    for (int* ptr = A; ptr < A + (sizeof(A) / sizeof(A[0])); ++ptr) {
+    cout << "DESPUES:  ";
+    for (int* ptr = A; ptr <= q; ++ptr) {
         cout << *ptr << " ";
     }
     cout << endl;
