92b8b2fc8cb3695d2dce10edb789ff04d4ab06f5
FerPonce42 <angel.pacsi@ucsp.edu.pe>
2024-09-30 07:37:40 -0500

    Split War is Over

--- TAREAS/tarea4/split.cpp
+++ TAREAS/tarea4/split.cpp
@@ -1,25 +1,36 @@
 #include <iostream>
 using namespace std;
 
-void split(int* p, int* q) {
-    int* r = p; 
-    while (p <= q) { 
-        if (*p % 2 == 0) {
-            swap(*p, *r);
-            r++;
-            p++;
+void split(int *p, int *q) {
+    
+    for (int *f = p; f <= q; f++) { 
+        
+        if (*p % 2 == 1) { 
+            for (int *r = q; r > p; r--) { 
+                swap(*p, *r); 
+            }
         } else {
-            p++;
+            p++; 
         }
     }
 }
 
 int main() {
     int A[] = {1, 2, 3, 4, 5, 6, 7, 8}; 
-    int* p = A;
-    int* q = A + (sizeof(A) / sizeof(A[0])) - 1;
+    int* p = A; 
+    int* q = A + (sizeof(A) / sizeof(A[0])) - 1; 
+
+    cout << "ANTES:  ";
+    for (int* ptr = p; ptr <= q; ++ptr) {
+        cout << *ptr << " ";
+    }
+    cout << endl;
 
     split(p, q);
 
-    return 0;
+    cout << "DESPUES: ";
+    for (int* ptr = A; ptr <= q; ++ptr) {
+        cout << *ptr << " ";
+    }
+    cout << endl;
 }
